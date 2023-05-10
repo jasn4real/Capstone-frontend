f2addab64be205ec8a5ef3e89c52168f20714982
hector918 <hector_zhong@outlook.com>
2023-05-09 20:49:32 -0400

    add fetch functions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
 import About from "./pages/About";
 import Navigation from "./components/Navigation";
+import { useEffect } from "react";
+// import srv from './fetch_';
+
+// console.log(srv.read_text_to_image("a bottle of water"));
 
 export default function App() {
   return (

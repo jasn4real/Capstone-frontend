72ed64f4542d759efdbaf4d68f44d218b01d2973
Karyn Huston <huston.karyn@gmail.com>
2023-05-13 10:51:57 -0400

    set book data

--- src/App.js
+++ src/App.js
@@ -3,47 +3,50 @@ import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
 import About from "./pages/About";
 import Navigation from "./components/Navigation";
-
-import srv from './fetch_';
-
-
-// const upload_ = (metadata)=>{
-//   let books = localStorage.getItem("books");
-//   books = books.push(metadata);
-//   localStorage.setItem("books", books);
-// }
-
-// ()=>{
-//   localStorage.setItem("books",[
-//     {bookID: "book-{filehash}"},
-//     {bookID: "book-{filehash}"}
-//   ]);
-// }
-
-// ()=>{
-//   const books = localStorage.getItem("books");
-//   const book = localStorage.getItem(`book-${books[0].bookID}`);
-
-// }
-
-// srv.read_text_to_image("a bottle of water", (question, data)=>{
-
-//   localStorage.setItem("book-{filehash}",{
-//     image_histroy:[
-//       {question: question, result: data }
-//     ],
-//     text_histroy:[
-
-//     ],
-//   });
-
-// });
-// ()=>{
-//   localStorage.getItem("book-{filehash}");//{question: question, result: data }
-// }
-
-
 import ReadingAssistance from "./pages/ReadingAssistance";
+
+// import srv from './fetch_';
+
+const existingBooks = JSON.parse(localStorage.getItem("books")) || [];
+
+//format book data
+const declareBooks = (metadata, data) => {
+  localStorage.setItem(
+    "books",
+    JSON.stringify([
+      { bookID: metadata.file_hash, book_data: metadata, results: data.result },
+    ])
+  );
+  localStorage.getItem("books");
+}
+
+
+  srv.read_text_to_image("a bottole of water", (question, data) => {
+
+  //set key of "hash" to local storage
+  //stringigy the second argument to store the data object as string into local storage.
+    localStorage.setItem("hash", JSON.stringify({question:question, result: data }));
+
+     //JSON.parse is used to retrieve the data object from local storage to access.
+     const storage_object = JSON.parse(localStorage.getItem("hash"))
+     console.log(storage_object)
+  })
+
+
+  srv.read_text_to_explanation((question, data) => {
+
+    //retrieve existing hash table or empty {}
+    const storedData = JSON.parse(localStorage.getItem("hash")) || {};
+
+    //assign a new key/pair to the object
+   
+        Object.assign(storedData, {asked: question}, {textR:data})
+
+    //store the updated object back to local storage
+    localStorage.setItem("hash",JSON.stringify(storedData));
+   console.log(storedData)
+    
+  })
 
 
 export default function App() {

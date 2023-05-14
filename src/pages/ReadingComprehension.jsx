ee9dce21b9a1b1e8f6f230077727f7015dd7c690
Karyn Huston <huston.karyn@gmail.com>
2023-05-14 11:55:56 -0400

    missing closing head element in html

--- src/pages/ReadingComprehension.jsx
+++ src/pages/ReadingComprehension.jsx
@@ -1,6 +1,22 @@
+import lc from '../storage_';
+
+
 export default function ReadingComprehension() {
 
+    // function displayFileDetails(){
+    //     const fileHash = '0ad1d820761a5aca9df52c22ea1cfc4ca5dad64923f51270dbe8f106f3817eef';
+    //     console.log(lc.getFileDetail(fileHash, 
+    //       [
+    //         'metaData',
+    //         'textToImage',
+    //         'textToExplanation',
+    //         'textToComprehenstion',
+    //       ]));
+    //   }
+      
+    //   displayFileDetails(); // call the function to display the file details
+
+
     return (
-        <p>reading comprehension</p>
-    )
+       <div className="Conatiner"><p>hello</p></div>    )
 }

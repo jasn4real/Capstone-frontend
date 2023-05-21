9ccee965957842f72bd0d71622f4ab90198748f8
Karyn Huston <huston.karyn@gmail.com>
2023-05-20 20:10:17 -0400

    re position form and reading level, history tab in progress

--- src/pages/ReadingComprehension.jsx
+++ src/pages/ReadingComprehension.jsx
@@ -27,43 +27,66 @@ export default function ReadingComprehension() {
   };
 
   return (
-    <div className="container rc-content">
-      <div className="container rc-text">
-        {" "}
-        {isLoading ? (
-          <div className="lds-ellipsis">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        ) : (
-          responseData && <div className="rc-text">{responseData}</div>
-        )}
-      </div>
-      <Form className="rc-form-box" onSubmit={handleReadingComprehensionSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control
-            type="text"
-            name="readingcomprehenstion"
-            placeholder="Type..."
-          />
-        </Form.Group>
-        <button className="rc-submit-button" type="submit">
-          SEND
-        </button>
-      </Form>
-      {/*  */}
+    <div>
+      <ul class="hList">
+        <li>
+          <a href="#click" class="menu">
+            <h2 class="menu-title">History</h2>
+            <ul class="menu-dropdown">
+              <li>cat</li>
+              <li>dog</li>
+              <li>horse</li>
+              <li>cow</li>
+              <li>pig</li>
+            </ul>
+          </a>
+        </li>
+      </ul>
 
-      <div className="reading-level-radio">
-        <span className="reading-level-text">Select reading level</span>
-        <div class="group">
-          <input type="radio" name="rb" id="rb1" />
-          <label for="rb1">Foundational</label>
-          <input type="radio" name="rb" id="rb2" />
-          <label for="rb2">Proficient</label>
-          <input type="radio" name="rb" id="rb3" />
-          <label for="rb3">Advanced</label>
+      <div className="container content">
+        <div className="reading-level-radio">
+          <span className="reading-level-text">Select reading level</span>
+
+          <div class="group">
+            <input type="radio" name="rb" id="rb1" />
+            <label for="rb1">Foundational</label>
+            <input type="radio" name="rb" id="rb2" />
+            <label for="rb2">Proficient</label>
+            <input type="radio" name="rb" id="rb3" />
+            <label for="rb3">Advanced</label>
+          </div>
+        </div>
+        <Form
+          className="rc-form-box"
+          onSubmit={handleReadingComprehensionSubmit}
+        >
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control
+              type="text"
+              name="readingcomprehenstion"
+              placeholder="what is this text about ?"
+            />
+          </Form.Group>
+          <button className="rc-submit-button" type="submit">
+            SEND
+          </button>
+        </Form>
+        {/*  */}
+      </div>
+
+      <div className="container rc-content">
+        <div className="container rc-text">
+          {" "}
+          {isLoading ? (
+            <div className="lds-ellipsis">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          ) : (
+            responseData && <div className="rc-text">{responseData}</div>
+          )}
         </div>
       </div>
     </div>

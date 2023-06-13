001090fe5e9d9070e97be6679de697835d394216
bigdaddyjean <jeannappoleon@pursuit.org>
2023-06-13 19:41:57 -0400

    merge

--- src/pages/fw-v0.02-sub/reading-assistance-page.js
+++ src/pages/fw-v0.02-sub/reading-assistance-page.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "./reading-assistance-page.css";
 import ComprehensionPage from "./reading-comprehension-page-h";
 import ReadingAssistance from "../ReadingAssistance";
-<<<<<<< Updated upstream
 
 let triggerHistoryUpdate_callback ;
 const setTriggerHistoryUpdate = ( func ) => {
@@ -12,22 +11,8 @@ const triggerHistoryUpdate = (q) =>{
   if(triggerHistoryUpdate_callback) triggerHistoryUpdate_callback(q);
 }
 export default function AssistancePage({pop_frame, fileHash}){
-
-=======
-import { getHistory } from "../../storage_";
-import {
-  highlighter,
-  image,
-  textplanation,
-  yellowsun,
-  bluemoon
-} from '../../userwalkthrough/index'
-
-export default function AssistancePage({ pop_frame, fileHash }) {
-  const [readingAssistanceWidth, setReadingAssistanceWidth] = useState(50);
   const [isNightModeActive, setIsNightModeActive] = useState(false);
-  
->>>>>>> Stashed changes
+
 
   function onLandingClick() {
     pop_frame(0);
@@ -57,24 +42,15 @@ export default function AssistancePage({ pop_frame, fileHash }) {
         <div className="reading-panel" style={{ flex: "50px" }}>
           <div className="text-reading-panel">
             <div className="popup-detail">
-<<<<<<< Updated upstream
               <ReadingAssistance fileHash={fileHash} triggerHistoryUpdate={triggerHistoryUpdate}/> 
-=======
-              <ReadingAssistance />
->>>>>>> Stashed changes
             </div>
           </div>
-          
         </div>
 
         <div className="comprehension-panel">
-<<<<<<< Updated upstream
-        <ComprehensionPage fh={fileHash} setTriggerHistoryUpdate={setTriggerHistoryUpdate} />
-=======
-          <ComprehensionPage fileHash={fileHash} />
+          <ComprehensionPage fh={fileHash} setTriggerHistoryUpdate={setTriggerHistoryUpdate} />
         </div>
->>>>>>> Stashed changes
       </div>
-    </div>
+    </div> // Missing in the original code
   );
 }
